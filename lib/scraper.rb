35eedca4b8b175ed0d2a560d409a4fc9f3f82c48
Chris Baum <christopher.baum@valpo.edu>
2021-02-10 01:59:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,12 +10,7 @@ class Scraper
   end
 
   def get_courses
-    @doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end
+    self.get_page.css(".post")
   end
 
   def make_courses
